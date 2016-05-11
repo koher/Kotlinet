ecee716e97c9d5563933a3196273b685885bb918
Yuta Koshizawa <koher@koherent.org>
2016-05-11 12:40:03 +0900

    Make `Result` a `sealed class`

--- project/app/src/lib/kotlin/org/koherent/kotlinet/Result.kt
+++ project/app/src/lib/kotlin/org/koherent/kotlinet/Result.kt
@@ -1,6 +1,6 @@
 package org.koherent.kotlinet
 
-open class Result<out Value> private constructor() {
+sealed class Result<out Value> private constructor() {
     class Success<out Value>(val value: Value) : Result<Value>() {}
     class Failure<out Value>(val bytes: ByteArray?, val exception: Exception) : Result<Value>() {}
 }
